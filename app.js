949a38a99c179ac35b1e20db48b94068817fc400
ChristopherZalapa <chriszalapa@gmail.com>
2021-02-10 10:25:09 -0800

    Calculating Js

---
+++ app.js
@@ -0,0 +1,8 @@
+// Listen For Submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+function calculateResults(e) {
+    console.log('Calculating');
+
+    e.preventDefault();
+}
