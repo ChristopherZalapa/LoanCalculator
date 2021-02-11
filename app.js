98f37af2d7ad560f7eb34d73a6b1f0f6b8b06430
ChristopherZalapa <chriszalapa@gmail.com>
2021-02-11 09:02:05 -0800

    Compute Monthly Payment

--- app.js
+++ app.js
@@ -11,7 +11,20 @@ function calculateResults(e) {
     const totalPayment = document.getElementById('total-payment');
     const totalInterest = document.getElementById('total-interest');
 
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
     
+    // Compute Monthly Payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+    if(isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    } else {
+
+    }
 
     e.preventDefault();
 }
