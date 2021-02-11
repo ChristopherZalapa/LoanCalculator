09de1c39af86f6e5128b757e556217a9ee48b90e
ChristopherZalapa <chriszalapa@gmail.com>
2021-02-11 08:45:52 -0800

    UI Vars

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ document.getElementById('loan-form').addEventListener('submit', calculateResults
 
 function calculateResults(e) {
     console.log('Calculating');
+    // UI Vars
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const amount = document.getElementById('amount');
 
     e.preventDefault();
 }
